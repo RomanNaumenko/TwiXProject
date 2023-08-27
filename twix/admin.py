c441ed949ae75cf0856d05a6d7e36ec32dd2f0a7
RomanNaumenko <lwoodooz7@gmail.com>
2023-08-27 18:06:58 +0300

    Git init. Create models and first basic views. Provided appropriate frontend(navbar, base page) and upgraded admin section.

---
+++ twix/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+
+# Register your models here.
+
+# Unregister your models here.
+admin.site.unregister(Group)
+
+
+# Mix Profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Just display username fields on display
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+# Reregister user
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
